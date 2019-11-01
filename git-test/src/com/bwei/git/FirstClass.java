517375b82c949747f875736327c4a23f38918489
feng <feng@itcast.cn>
2019-11-01 14:23:59 +0800

    把本地工程添加到git

---
+++ git-test/src/com/bwei/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.bwei.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
